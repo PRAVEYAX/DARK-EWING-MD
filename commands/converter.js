8c93876023f65d76819a7245335863dc5b967fc7
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-11-22 04:44:56 +0530

    😈 ＤＡＲＫ ＥＷＩＮＧ 😈

--- commands/converter.js
+++ commands/converter.js
@@ -1,20 +1,4 @@
-/**
-██╗███████╗██╗   ██╗██╗  ██╗██╗   ██╗    ███╗   ███╗██████╗ 
-██║╚══███╔╝██║   ██║██║ ██╔╝██║   ██║    ████╗ ████║██╔══██╗
-██║  ███╔╝ ██║   ██║█████╔╝ ██║   ██║    ██╔████╔██║██║  ██║
-██║ ███╔╝  ██║   ██║██╔═██╗ ██║   ██║    ██║╚██╔╝██║██║  ██║
-██║███████╗╚██████╔╝██║  ██╗╚██████╔╝    ██║ ╚═╝ ██║██████╔╝
-╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝     ╚═╝     ╚═╝╚═════╝ 
-                                                            
- Copyright (C) 2022.
- Licensed under the  GPL-3.0 License;
- You may not use this file except in compliance with the License.
- It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
- * @version 0.0.6
- **/
+
 
 const axios = require('axios')
 const { sck1, tiny, fancytext, listall,cmd,ffmpeg,Config } = require('../lib/')
@@ -219,7 +203,7 @@ cmd({
             pattern: "fancy",
             desc: "Makes stylish/fancy given text",
             category: "converter",
-            use: '46 Izuku',
+            use: '46 DARK',
             react: "✅",
             filename: __filename
         },
