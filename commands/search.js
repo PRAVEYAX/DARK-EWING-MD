0b69af1e652c73eeb8288a3ba9a364e217196c9c
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-11-23 00:27:55 +0530

    😈 ＤＡＲＫ ＥＷＩＮＧ 😈

--- commands/search.js
+++ commands/search.js
@@ -157,6 +157,8 @@ cmd({
     //---------------------------------------------------------------------------
 cmd({
             pattern: "image",
+            react: "📸",
+            alias :['img','imge'],
             category: "search",
             desc: "Searches Image on Google",
             use: '<text>',
@@ -166,25 +168,23 @@ cmd({
             if (!text) return citel.reply("Provide me a query!")
             if (!text) return reply("Hey bie please tell me for which pic you're looking");
             let name1 = text.split("|")[0]
-            let name2 = text.split("|")[1] || `1`
+            let name2 = text.split("|")[5] || `5`
             citel.reply(`Sending ${name2} image(s) of ${name1} in chat`)
             let nn = name2
             for (let i = 0; i < nn; i++) {
 
-                gis(name1, async(error, result) => {
-                    n = result;
-                    images = n[Math.floor(Math.random() * n.length)].url;
+                let n = await gis(name1)
+                images = n[Math.floor(Math.random() * n.length)].url;
                     let buttonMessage = {
                         image: {
                             url: images,
                         },
-                        caption: ` `,
+                        caption: `_📸 King Vajira Image Search 📸_\n*${name1}*`,
                         headerType: 4,
                     };
                     Void.sendMessage(citel.chat, buttonMessage, {
                         quoted: citel,
                     });
-                })
             }
         }
     )
